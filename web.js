d48f16e9977c208a6ba0db1084f4f11300847e1e
Michał Faleński <sket_fala@o2.pl>
2013-07-09 22:15:53 +0000

    fs added

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var fs=require("fs");
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var out = fs.readFileSync("index.html", 'utf8');
+  var out = fs.readFileSync('index.html', 'utf8');
   response.send(out.toString());
 });
 
