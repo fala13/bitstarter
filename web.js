65692d07771c9f9c8f3f58d155e597ea9b49ba1f
Michał Faleński <sket_fala@o2.pl>
2013-07-09 22:29:31 +0000

    ble

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var out = fs.readFileSync('index.html', 'utf8');
-  response.send(out.toString());
+  response.send(out);
 });
 
 var port = process.env.PORT || 5000;
